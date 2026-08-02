a180867013dc26c8aabc32bec4086b5614ee144b
Jason Hall <jasonbrianhall@gmail.com>
2026-08-02 14:38:00 -0400

    Latest

--- gtk3/audioconverter.h
+++ gtk3/audioconverter.h
@@ -2,7 +2,7 @@
 #define AUDIO_CONVERTER_H
 
 #include <vector>
-
+#include <cstdint>
 // Function to convert MP3 data to WAV data in memory
 bool convertMp3ToWavInMemory(const std::vector<uint8_t>& mp3Data, std::vector<uint8_t>& wavData);
 
